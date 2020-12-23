d68be7e6172e04420e22439c7d3e624c28d12980
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2020-12-23 14:31:22 -0300

    Add css e templates

--- web.py
+++ web.py
@@ -3,19 +3,21 @@ from actions import list_marketplaces_name, list_marketplace_categories, list_ca
 
 app = Flask(__name__)
 
+titulo_head = 'Marketplaces'
+
 @app.route('/')
 def home():
     resultado = list_marketplaces_name()
-    return render_template('home.html', lista = resultado)
+    return render_template('home.html', titulo_head = titulo_head, lista = resultado, titulo = 'Marketplaces')
 
 @app.route('/marketplace/<name>')
 def marketplace(name):
     resultado = list_marketplace_categories(name)
-    return render_template('marketplace.html', lista = resultado, nome = name)
+    return render_template('marketplace.html', titulo_head = titulo_head, lista = resultado, nome = name, titulo = 'Marketplace')
 
 @app.route('/categoria/<name>')
 def category(name):
     resultado = list_categorie_subcategories(name)
-    return render_template('category.html', lista = resultado, nome = name)
+    return render_template('category.html', titulo_head = titulo_head, lista = resultado, nome = name, titulo = 'Categoria')
 
 app.run(debug=True)
